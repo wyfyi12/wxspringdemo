cf185e9895e257802522568710ffce06cc25f927
wyfyi12 <wyfyi12@sina.com>
2016-12-30 15:59:14 +0800

    2016年12月30日15:59:09

---
+++ src/com/entities/TokenEntity.java
@@ -0,0 +1,12 @@
+package com.entities;
+
+import java.io.Serializable;
+
+public class TokenEntity implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
